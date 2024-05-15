76c810e43db41b8028c89d14aedf64dadbecef0f
SaiDeepthiSamineni <168463386+SaiDeepthiSamineni@users.noreply.github.com>
2024-05-15 12:51:24 +0530

    Add files via upload

---
+++ Approach2.java
@@ -0,0 +1,14 @@
+package com.si.sample23;
+public class Approach2{
+	String batsmen="Dhoni";
+	static String bowler="Pathirana";
+	void display() {
+		System.out.println("using Approch2");
+			
+
+	}
+	static String display2() {
+		return "static display2";
+	}
+
+}
---
+++ userdefinedpackage.java
@@ -0,0 +1,32 @@
+package ipl;
+class Srh{
+	void bowler() {
+		System.out.println("srh bowler");
+	}
+
+
+	void batsmen() {
+		System.out.println("srh batsmen");
+	}
+}
+class Rcb{
+	void bowler() {
+		System.out.println("rcb bowler");
+	}
+	void batsmen() {
+		System.out.println("rcb batsmen");
+	}
+	
+}
+class Csk{
+	void bowler() {
+		System.out.println("csk bowler");
+
+		
+	}
+	void batsmen() {
+		System.out.println("csk bowler");
+
+	}
+}
+
