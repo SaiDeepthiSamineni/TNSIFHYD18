4ac1c18b5f74295f552622053de6f6ae25e56563
SaiDeepthiSamineni <168463386+SaiDeepthiSamineni@users.noreply.github.com>
2024-05-15 12:50:23 +0530

    Add files via upload

---
+++ Access.java
@@ -0,0 +1,20 @@
+package com.si.sample23;
+
+	public class Access{
+		
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Approach2 a2=new Approach2();
+			System.out.println(a2.batsmen);
+			a2.display();
+			System.out.println(Approach2.bowler);
+			
+			
+			
+
+		}
+
+	}
+
+
