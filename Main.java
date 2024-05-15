160102b4695708a466889ed56b1b4cf7ab4505f8
SaiDeepthiSamineni <168463386+SaiDeepthiSamineni@users.noreply.github.com>
2024-05-15 13:02:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package ipl;
+
+public class Main {
+	public static void main(String[] args) {
+		Srh s1=new Srh();
+		Rcb r1=new Rcb();
+		Csk c1=new Csk();
+		s1.bowler();
+		s1.batsmen();
+		r1.bowler();
+		r1.batsmen();
+		c1.bowler();
+		c1.batsmen();
+	}
+
+}
---
+++ package-info.java
@@ -0,0 +1,36 @@
+package ipl;
+class Srh{
+	void bowler() {
+		System.out.println("srh bowler");
+		
+	}
+	void batsmen() {
+		System.out.println("srh batsmen");
+		
+	}
+}
+class Rcb {
+	void bowler() {
+		System.out.println("rcb bowler");
+
+		
+	}
+	void batsmen() {
+		System.out.println("rcb batsmen");
+
+		
+	}
+}
+class Csk{
+	void bowler() {
+		System.out.println("Csk bowler");
+
+		
+	}
+	void batsmen() {
+		System.out.println("Csk batsmen");
+
+		
+	}
+	
+}
